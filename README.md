49d1c551a845415e65f052299a322a0f73e2fe6e
Jette <dohnjette.deguzman@gmail.com>
2022-08-23 00:33:01 +0800

    Added button and score to the html and modified the whole JS

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Rock_Paper_Scissors
+
+08/16/2022 - The repository was uploaded with a very basic functionality.
+the player was asked for the selection via prompt, and the result is displayed by console log.
+
+08/23/2022 - I edited the html and js, I added buttons and display for the score, the whole javascript code was edited. Whoever is the first one to get the score of 5 wins the game. Is any of the player wins, the selection button will be disabled and the reset game button will show.
--- index.js
+++ index.js
@@ -1,36 +1,103 @@
-// generate the computer choice
-const choices = ["Rock", "Paper", "Scissors"];
+let userScore = 0;
+let compScore = 0;
+const userScoreLog = document.querySelector(".playerScore");
+const computerScoreLog = document.querySelector(".computerScore");
+const result = document.querySelector(".finalScore > p");
+const chooseRock = document.getElementById("btn-Rock");
+const choosePaper = document.getElementById("btn-Paper");
+const chooseScissors = document.getElementById("btn-Scissors");
+const resetGameButton = document.getElementById("resetGame");
+let playerChoice;
 
-function getComputerChoice(choices) {
-   return choices[Math.floor(Math.random() * choices.length)];
+chooseRock.addEventListener('click', () => game("ROCK"));
+choosePaper.addEventListener('click', () => game("PAPER"));
+chooseScissors.addEventListener('click', () => game("SCISSORS"));
+
+// Player Choice
+function game(playerSelection) {
+    //playerChoice = playerSelection;
+    const computerSelection = getComputerChoice();
+    
+    //this will call a function based on the result of the comparison between the player and computer choice
+    if (playerSelection === computerSelection) {
+        draw(playerSelection, computerSelection);
+      } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS'){
+        playerWin(playerSelection, computerSelection);  
+      }else if (playerSelection === 'PAPER' && computerSelection === 'ROCK'){
+        playerWin(playerSelection, computerSelection);
+      }else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER'){
+        playerWin(playerSelection, computerSelection);
+      }else{
+        computerWin(playerSelection, computerSelection);
+      }   
+
+}
+
+// computer random selection
+function getComputerChoice() {
+
+const choices = ["ROCK", "PAPER", "SCISSORS"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+// if player wins the game, add player score by 1, if user score is equal to 5, show reset game button
+function playerWin(playerChoice, computerChoice) {
+    userScore++;
+    userScoreLog.innerHTML = userScore;
+    computerScoreLog.innerHTML = compScore;
+
+    if (userScore < 5){result.innerHTML = `${playerChoice} beats ${computerChoice}. You win! =D`;
+}else if(userScore === 5){
+  result.innerHTML="Player win the game";
+  showResetButton();
+
+  chooseRock.setAttribute("disabled", 1);
+  choosePaper.setAttribute("disabled", 1);
+  chooseScissors.setAttribute("disabled", 1);
+}; 
+}
+
+function computerWin(playerChoice, computerChoice) {
+    compScore++;
+    userScoreLog.innerHTML = userScore;
+    computerScoreLog.innerHTML = compScore;
+
+    if (compScore < 5){result.innerHTML = `${computerChoice} beats ${playerChoice}. You lose!`;
+}else if(compScore === 5){
+    result.innerHTML="Computer win the game";
+    showResetButton();
+
+  chooseRock.setAttribute("disabled", 1);
+  choosePaper.setAttribute("disabled", 1);
+  chooseScissors.setAttribute("disabled", 1);
+}; 
+}
+
+function draw() {
+    userScoreLog.innerHTML = userScore;
+    computerScoreLog.innerHTML = compScore;
+    result.innerHTML = `It\'s a tie!`;
 }
 
 
-let upperText = prompt('Select your weapon! Rock, Paper or Scissors!'); // prompt to ask for user's selection
-let playerSelection = upperText.toUpperCase(); //this converts any case to uppercase
-const computerSelection = getComputerChoice(choices).toUpperCase(); // this converts the computer selection to uppercase
-let resultText; //the battle result is stored in this variable
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection){
-        resultText = "Its a tie!";
+//show reset game button
+function showResetButton(){
+    if (resetGameButton.style.display === "none") {
+        resetGameButton.style.display = "block";
+    } else {
+        resetGameButton.style.display = "none";
     }
-    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
-        resultText = "You Win!";
-    }
-    else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
-        resultText = "You Win!";
-    }
-    else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
-        resultText = "You Win!";
-    }
-    else {
-        resultText ="You Lose!"
-    }
-    return resultText;
   }
 
-console.log(playerSelection);
-console.log(computerSelection);
-
-console.log(playRound(playerSelection, computerSelection));
+  // Button to reset the whole game
+function resetGame() {
+    userScore = 0;
+    compScore = 0;
+    userScoreLog.innerHTML = userScore;
+    computerScoreLog.innerHTML = compScore;
+    result.innerHTML = ``;
+    chooseRock.removeAttribute("disabled");  
+    choosePaper.removeAttribute("disabled");  
+    chooseScissors.removeAttribute("disabled");
+    resetGameButton.style.display = "none"
+  }
